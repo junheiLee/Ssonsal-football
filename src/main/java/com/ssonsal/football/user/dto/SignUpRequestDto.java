195dcaf2760fabfbd7f9e9694d14d6c881e5c9e1
Caras <caras6318@gmail.com>
2023-12-01 19:22:17 +0900

    Feat : SignUp, SignIn 기능구현

---
+++ src/main/java/com/ssonsal/football/user/dto/SignUpRequestDto.java
@@ -0,0 +1,37 @@
+package com.ssonsal.football.user.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class SignUpRequestDto {
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String birth;
+
+    private int gender;
+
+    private String nickname;
+
+    private String position;
+
+    private String phone;
+
+    private String preferredTime;
+
+    private String preferredArea;
+
+    private String intro;
+
+    private int role;
+}
