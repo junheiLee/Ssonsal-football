2b202848002eee96c45e2a71b8f64d41ac39a9a4
clclt <clclclclclt@naver.com>
2023-12-02 03:43:54 +0900

    Remove: 어노테이션 삭제

--- src/main/java/com/ssonsal/football/team/entity/TeamRecord.java
+++ src/main/java/com/ssonsal/football/team/entity/TeamRecord.java
@@ -4,8 +4,6 @@ import com.ssonsal.football.global.entity.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 
@@ -13,7 +11,6 @@ import javax.persistence.*;
 @Getter
 @Table(name = "team_record")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@DynamicInsert
 public class TeamRecord extends BaseEntity {
 
     @Id
@@ -24,22 +21,16 @@ public class TeamRecord extends BaseEntity {
     @JoinColumn(name = "team_id")
     private Team team;
 
-    @ColumnDefault("0")
     private Integer point;
 
-    @ColumnDefault("0")
     private Integer winCount;
 
-    @ColumnDefault("0")
     private Integer drawCount;
 
-    @ColumnDefault("0")
     private Integer loseCount;
 
-    @ColumnDefault("0")
     private Integer totalGameCount;
 
-    @ColumnDefault("-1")
     private Integer rank;
 
     public TeamRecord(Team team) {
