6f7de8312aa9059d359f72e01b9c134dc1e16bf4
clclt <clclclclclt@naver.com>
2023-12-01 15:52:46 +0900

    Feat: 모든 팀 목록 보기 기능 추가

---
+++ src/main/java/com/ssonsal/football/team/controller/TeamController.java
@@ -0,0 +1,43 @@
+package com.ssonsal.football.team.controller;
+
+import com.ssonsal.football.team.entity.Role;
+import com.ssonsal.football.team.service.MemberService;
+import com.ssonsal.football.team.service.TeamService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teams")
+@RequiredArgsConstructor
+@Slf4j
+public class TeamController {
+
+    private final TeamService teamService;
+    private final MemberService memberService;
+
+    /**
+     * 모든 팀 리스트를 가져온다.
+     *
+     * @return 팀 목록 정보 리스트
+     */
+    @GetMapping
+    public String showList(Model model) {
+
+        // 추후 토큰값으로 교체할 부분임
+        Long user = 1L;
+
+        if (user == null) {
+            model.addAttribute("userlevel", Role.GUEST);
+        } else {
+            model.addAttribute("userlevel", memberService.isUserLevel(user));
+        }
+
+        model.addAttribute("teams", teamService.findList());
+
+        return "teamList";
+    }
+}
--- src/main/java/com/ssonsal/football/team/service/MemberServiceImpl.java
+++ src/main/java/com/ssonsal/football/team/service/MemberServiceImpl.java
@@ -1,4 +1,64 @@
 package com.ssonsal.football.team.service;
 
-public class MemberServiceImpl implements MemberService{
+import com.ssonsal.football.team.entity.Role;
+import com.ssonsal.football.team.repository.TeamApplyRepository;
+import com.ssonsal.football.team.repository.TeamRejectRepository;
+import com.ssonsal.football.team.repository.TeamRepository;
+import com.ssonsal.football.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
+    private final TeamApplyRepository teamApplyRepository;
+    private final TeamRejectRepository teamRejectRepository;
+
+    /**
+     * 유저의 현재 직책에 따라 다른 값을 넘겨준다.
+     *
+     * @param userId 유저 아이디
+     * @return 유저 권한
+     */
+    @Override
+    public Role isUserLevel(Long userId) {
+
+        if (isUserOtherApply(userId)) {
+            return Role.TEAM_APPLY;
+        } else if (isUserTeamExists(userId)) {
+            return Role.USER;
+        }
+
+        return Role.TEAM_MEMBER;
+    }
+
+    /**
+     * 유저가 특정 팀에 신청 중 인지 확인합니다.
+     *
+     * @param userId 유저 아이디
+     */
+    @Override
+    public boolean isUserOtherApply(Long userId) {
+
+        return teamApplyRepository.existsById(userId);
+    }
+
+    /**
+     * 유저가 팀을 가지고 있는지 확인합니다. -> null 일때 true
+     *
+     * @param userId 유저 아이디
+     */
+    @Override
+    public boolean isUserTeamExists(Long userId) {
+
+        return userRepository.existsByIdAndTeamIsNull(userId);
+    }
+
 }
