875198ab1a3bd3fc151c70b250eafeaa433bc6ba
syl0415 <137062383+syl0415@users.noreply.github.com>
2023-12-01 16:05:53 +0900

    Feat: Review & Report 엔티티

---
+++ src/main/java/com/ssonsal/football/review/etity/Report.java
@@ -0,0 +1,38 @@
+package com.ssonsal.football.review.etity;
+
+import com.ssonsal.football.global.entity.BaseEntity;
+import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "report")
+public class Report extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    @NotNull
+    private Review review;
+
+    private String reason;
+    private int reportCode;
+
+    @Builder
+    public Report(Long id, Review review, String reason, int reportCode) {
+        this.id = id;
+        this.review = review;
+        this.reason = reason;
+        this.reportCode = reportCode;
+    }
+}
+
