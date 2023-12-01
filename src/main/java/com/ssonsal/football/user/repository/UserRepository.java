31be8a37d0fae1a2aa691df98c91762761790708
clclt <clclclclclt@naver.com>
2023-12-01 21:58:30 +0900

    Refactor: 불필요한 주석제거

--- src/main/java/com/ssonsal/football/user/repository/UserRepository.java
+++ src/main/java/com/ssonsal/football/user/repository/UserRepository.java
@@ -1,16 +1,9 @@
 package com.ssonsal.football.user.repository;
 
-import com.ssonsal.football.team.dto.response.TeamMemberListDto;
 import com.ssonsal.football.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
 
 
 }
