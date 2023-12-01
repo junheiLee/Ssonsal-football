47e27840e2aefb0473e7727de77f1745d84fabc8
clclt <clclclclclt@naver.com>
2023-12-01 22:01:32 +0900

    Refactor: 간격조절

--- src/main/java/com/ssonsal/football/user/repository/UserRepository.java
+++ src/main/java/com/ssonsal/football/user/repository/UserRepository.java
@@ -4,6 +4,4 @@ import com.ssonsal.football.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
 }
