88ccc18e4c3657d63493e7f36dbb1c27db80a856
syl0415 <137062383+syl0415@users.noreply.github.com>
2023-12-08 18:37:07 +0900

    Refactor: Review 컨트롤러 리팩토링

--- src/main/java/com/ssonsal/football/review/controller/ReviewController.java
+++ src/main/java/com/ssonsal/football/review/controller/ReviewController.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,24 +24,24 @@ public class ReviewController {
     @PostMapping
     public ResponseEntity<ResponseBodyFormatter> createReview(
             @RequestBody ReviewRequestDto reviewRequestDto) {
-        ReviewResponseDto createdReview = reviewService.createReview(reviewRequestDto);
-        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, createdReview);
+
+        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, reviewService.createReview(reviewRequestDto));
     }
 
     // 검색한 팀아이디에 대한 리뷰들 조회
     @GetMapping("/team/{teamId}")
     public ResponseEntity<ResponseBodyFormatter> getTeamReview(@PathVariable("teamId") Long teamId){
         log.info(String.valueOf(teamId));
-        List<ReviewResponseDto> result = reviewService.teamReviewList(teamId);
-        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, result);
+
+        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, reviewService.teamReviewList(teamId));
     }
 
     // 검색한 용병아이디에 대한 리뷰들 조회
     @GetMapping("/sub/{userId}")
     public ResponseEntity<ResponseBodyFormatter> getUserReview(@PathVariable("userId") Long userId){
         log.info(String.valueOf(userId));
-        List<ReviewResponseDto> result = reviewService.userReviewList(userId);
-        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, result);
+
+        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, reviewService.userReviewList(userId));
     }
 
     // 삭제코드 변경
@@ -58,7 +57,7 @@ public class ReviewController {
     @GetMapping("/review/{reviewId}")
     public ResponseEntity<ResponseBodyFormatter> getReview(@PathVariable("reviewId") Long reviewId){
         log.info(String.valueOf(reviewId));
-        ReviewResponseDto result = reviewService.getReview(reviewId);
-        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, result);
+
+        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, reviewService.getReview(reviewId));
     }
 }
