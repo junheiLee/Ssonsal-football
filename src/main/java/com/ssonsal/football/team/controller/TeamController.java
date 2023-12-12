383703dabf24dc051f979c93d66a8fccb8f1d8e8
clclt <clclclclclt@naver.com>
2023-12-13 01:00:37 +0900

    Refactor: 기존 model을 반환하는 구조에서 데이터만 반환해주게 수정

--- src/main/java/com/ssonsal/football/team/controller/TeamController.java
+++ src/main/java/com/ssonsal/football/team/controller/TeamController.java
@@ -1,5 +1,6 @@
 package com.ssonsal.football.team.controller;
 
+import com.ssonsal.football.game.util.Transfer;
 import com.ssonsal.football.global.exception.CustomException;
 import com.ssonsal.football.global.util.formatter.DataResponseBodyFormatter;
 import com.ssonsal.football.global.util.formatter.ResponseBodyFormatter;
@@ -13,166 +14,156 @@ import com.ssonsal.football.team.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ssonsal.football.game.util.Transfer.objectToMap;
+import static com.ssonsal.football.team.util.TeamConstant.*;
+import static com.ssonsal.football.team.util.Transfer.objectToMap;
 
 
-@Controller
+@RestController
 @RequestMapping("/teams")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class TeamController {
 
     private final TeamService teamService;
     private final MemberService memberService;
 
     /**
-     * 모든 팀 리스트를 가져온다.
+     * 모든 팀 리스트를 반환한다.
      *
      * @return 팀 목록 정보 리스트
      */
     @GetMapping
-    public String findAllTeams(Model model) {
+    public ResponseEntity<ResponseBodyFormatter> findAllTeams() {
 
         // 추후 토큰값으로 교체할 부분임
         Long user = 1L;
 
+        String userLevel = "";
+
         if (user == null) {
-            model.addAttribute("userlevel", Role.GUEST);
+            userLevel = Role.GUEST.getRole();
         } else {
-            model.addAttribute("userlevel", memberService.isUserLevel(user));
+            userLevel = memberService.isUserLevel(user).getRole();
         }
 
-        model.addAttribute("teams", teamService.findAllTeams());
-
-        return "team/teamList";
+        return DataResponseBodyFormatter.put(TeamSuccessCode.DATA_TRANSFER_SUCCESS, objectToMap(TEAMS, USER_LEVEL, teamService.findAllTeams(), userLevel));
     }
 
     /**
-     * 모집중인 팀 리스트를 가져온다.
+     * 모집중인 팀 리스트를 반환한다.
      *
      * @return 모집중인 팀 목록 정보 리스트
      */
     @GetMapping("/recruit")
-    public String findAllRecruitTeams(Model model) {
-
-        model.addAttribute("teams", teamService.findRecruitList());
+    public ResponseEntity<ResponseBodyFormatter> findAllRecruitTeams() {
 
         // 추후 토큰값으로 교체할 부분임
         Long user = 1L;
 
+        String userLevel = "";
+
         if (user == null) {
-            model.addAttribute("userlevel", Role.GUEST);
+            userLevel = Role.GUEST.getRole();
         } else {
-            model.addAttribute("userlevel", memberService.isUserLevel(user));
+            userLevel = memberService.isUserLevel(user).getRole();
         }
 
-        return "team/teamList";
+        return DataResponseBodyFormatter.put(TeamSuccessCode.DATA_TRANSFER_SUCCESS, objectToMap(TEAMS, USER_LEVEL, teamService.findRecruitList(), userLevel));
     }
 
     /**
-     * 특정 키워드를 주면, 키워드에 맞는 팀 이름을 찾아서 가져온다.
+     * 특정 키워드를 주면, 키워드에 맞는 팀 이름을 찾아서 반환한다.
      *
      * @param keyword 검색어
      * @return 팀명 검색어에 맞는 팀 목록 정보 리스트
      */
     @GetMapping("/search")
-    public String findAllSearchTeams(@RequestParam String keyword, Model model) {
-
-        model.addAttribute("teams", teamService.findSearchList(keyword));
+    public ResponseEntity<ResponseBodyFormatter> findAllSearchTeams(@RequestParam String keyword) {
 
         // 추후 토큰값으로 교체할 부분임
         Long user = 1L;
 
+        String userLevel = "";
+
         if (user == null) {
-            model.addAttribute("userlevel", Role.GUEST);
+            userLevel = Role.GUEST.getRole();
         } else {
-            model.addAttribute("userlevel", memberService.isUserLevel(user));
+            userLevel = memberService.isUserLevel(user).getRole();
         }
 
-        return "team/teamList";
+        return DataResponseBodyFormatter.put(TeamSuccessCode.DATA_TRANSFER_SUCCESS, objectToMap(TEAMS, USER_LEVEL, teamService.findSearchList(keyword), userLevel));
     }
 
     /**
-     * 특정 팀 상세정보를 가져온다.
+     * 특정 팀 상세정보를 반환한다.
      *
      * @param teamId 팀 아이디
      * @return 팀 아이디에 맞는 팀 정보
      */
     @GetMapping("/{teamId}")
-    public String findDetailOfTeam(@PathVariable Long teamId, Model model) {
+    public ResponseEntity<ResponseBodyFormatter> findDetailOfTeam(@PathVariable Long teamId) {
 
         // 추후 토큰값으로 교체할 부분임
         Long user = 1L;
 
+        String userLevel = "";
+
         if (user == null) {
-            model.addAttribute("userlevel", Role.GUEST);
+            userLevel = Role.GUEST.getRole();
         } else {
-            model.addAttribute("userlevel", memberService.isUserLevel(teamId, user));
+            userLevel = memberService.isUserLevel(teamId, user).getRole();
         }
 
-        model.addAttribute("detail", teamService.findTeamDetail(teamId));
-        model.addAttribute("members", teamService.findMemberList(teamId));
+        Map<String, Object> details = new HashMap<>();
+        details.put("userLevel", userLevel);
+        details.put("detail", teamService.findTeamDetail(teamId));
+        details.put("members", teamService.findMemberList(teamId));
 
-        return "team/teamDetail";
+        return DataResponseBodyFormatter.put(TeamSuccessCode.DATA_TRANSFER_SUCCESS, details);
     }
 
     /**
-     * 팀 신청자 목록과 팀 회원목록을 가져온다.
+     * 팀 관리 목록을 반환한다.
      *
      * @param teamId 팀 아이디
-     * @return 팀 아이디에 맞는 회원 정보와 팀 신청자 정보
+     * @return 팀 아이디에 맞는 회원 정보와 팀 신청자 정보와 밴 유저 목록
      */
     @GetMapping("/{teamId}/managers")
-    public String findManageListOfTeam(@PathVariable Long teamId, Model model) {
+    public ResponseEntity<ResponseBodyFormatter> findManageListOfTeam(@PathVariable Long teamId) {
 
         // 추후 토큰값으로 교체할 부분임
         Long user = 1L;
 
         if (user == null) {
-            return "index";
+            throw new CustomException(TeamErrorCode.USER_NOT_AUTHENTICATION);
         } else {
             if (!memberService.isTeamLeader(teamId, user)) {
-                return "index";
+                throw new CustomException(TeamErrorCode.MEMBER_NOT_LEADER);
             }
         }
 
-        model.addAttribute("manage", teamService.findManageList(teamId));
-
-        return "team/teamManage";
+        return DataResponseBodyFormatter.put(TeamSuccessCode.DATA_TRANSFER_SUCCESS, teamService.findManageList(teamId));
     }
 
     /**
-     * 신규 팀을 생성하는 페이지로 이동한다.
-     */
-    @GetMapping("/form")
-    public String createForm() {
-
-        Long user = 1L;
-
-        if (user == null) {
-            return "login";
-        } else if (memberService.hasAnyTeam(user)) {
-            throw new CustomException(TeamErrorCode.MEMBER_ALREADY_TEAM);
-        }
-
-        return "teamCreate";
-    }
-
-    /**
-     * 팀을 생성한다.
+     * 팀을 생성하고 팀 번호와 이름을 반환한다.
      *
-     * @param teamCreateDto
+     * @param teamCreateDto 유저가 입력한 form 데이터
      * @return 생성된 팀 번호,팀 명
      */
     @PostMapping
     @ResponseBody
     public ResponseEntity<ResponseBodyFormatter> createTeam(@Valid @ModelAttribute TeamCreateDto teamCreateDto) {
 
-        Long user = 11L;
+        Long user = 1L;
 
         if (user == null) {
             throw new CustomException(TeamErrorCode.USER_NOT_AUTHENTICATION);
@@ -188,31 +179,29 @@ public class TeamController {
     }
 
     /**
-     * 팀 정보 수정 페이지를 불러온다.
+     * 기존 팀 정보를 반환한다.
      *
-     * @param teamId
-     * @return teamEditFormDto 팀 정보 DTO
+     * @param teamId 팀 아이디
+     * @return teamEditFormDto 기존 팀 정보 DTO
      */
     @GetMapping("/{teamId}/edit")
-    public String editForm(Model model, @PathVariable Long teamId) {
+    public ResponseEntity<ResponseBodyFormatter> editForm(@PathVariable Long teamId) {
 
         Long user = 1L;
 
         if (user == null) {
-            return "login";
+            throw new CustomException(TeamErrorCode.USER_NOT_AUTHENTICATION);
         } else if (!memberService.isTeamLeader(teamId, user)) {
-            return "redirect:/teams/" + teamId;
+            throw new CustomException(TeamErrorCode.MEMBER_NOT_LEADER);
         }
 
-        model.addAttribute("form", teamService.findTeamInfo(teamId));
-
-        return "teamEdit";
+        return DataResponseBodyFormatter.put(TeamSuccessCode.DATA_TRANSFER_SUCCESS, objectToMap(FORM, teamService.findTeamInfo(teamId)));
     }
 
     /**
-     * 팀 정보를 수정한다.
+     * 팀 정보를 수정하고 ID값을 반환한다.
      *
-     * @param teamEditDto
+     * @param teamEditDto 팀 수정 form 데이터
      * @return 팀 아이디 값
      */
     @PatchMapping
@@ -229,6 +218,6 @@ public class TeamController {
             throw new CustomException(TeamErrorCode.DUPLICATE_TEAM_NAME);
         }
 
-        return DataResponseBodyFormatter.put(TeamSuccessCode.LEADER_EDIT_SUCCESS, teamService.editTeam(teamEditDto));
+        return DataResponseBodyFormatter.put(TeamSuccessCode.LEADER_EDIT_SUCCESS, Transfer.longIdToMap(TEAM_ID, teamService.editTeam(teamEditDto)));
     }
 }
