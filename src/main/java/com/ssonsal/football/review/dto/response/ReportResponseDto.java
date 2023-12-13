c2f8a21f0faf0edf8864f8392bbe06c70430253f
syl0415 <137062383+syl0415@users.noreply.github.com>
2023-12-13 14:38:24 +0900

    Refactor: ReportRespnseDto 변경

--- src/main/java/com/ssonsal/football/review/dto/response/ReportResponseDto.java
+++ src/main/java/com/ssonsal/football/review/dto/response/ReportResponseDto.java
@@ -1,24 +1,31 @@
 package com.ssonsal.football.review.dto.response;
 
 import com.ssonsal.football.review.etity.Report;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
+@Builder
 public class ReportResponseDto {
     private Long id;
     private Long reviewId;
     private String reason;
     private int reportCode;
+    private LocalDate createdAt;
+    private LocalDate modifiedAt;
 
     public static ReportResponseDto fromEntity(Report report) {
-        ReportResponseDto reportResponseDto = new ReportResponseDto();
-        reportResponseDto.setId(report.getId());
-        reportResponseDto.setReviewId(report.getReview().getId());
-        reportResponseDto.setReason(report.getReason());
-        reportResponseDto.setReportCode(report.getReportCode());
-        return reportResponseDto;
+        return new ReportResponseDtoBuilder()
+                .id(report.getId())
+                .reviewId(report.getReview().getId())
+                .reason(report.getReason())
+                .reportCode(report.getReportCode())
+                .createdAt(LocalDate.from(report.getCreatedAt()))
+                .modifiedAt(LocalDate.from((report.getModifiedAt())))
+                .build();
     }
 }
-
