2031cba1d5b96aa60ee1e0bce0f946ef005180c7
clclt <clclclclclt@naver.com>
2023-12-13 14:29:21 +0900

    Feat: 요청 경로 추가

--- src/main/java/com/ssonsal/football/team/controller/MemberController.java
+++ src/main/java/com/ssonsal/football/team/controller/MemberController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/members")
 @Tag(name = "Member", description = "Member API")
 public class MemberController {
 
