c9dc0e161c02bcc7cf74b095869e301dd4c5d492
syl0415 <137062383+syl0415@users.noreply.github.com>
2023-12-13 17:06:34 +0900

    Refactor: 코드리팩토링

--- src/main/java/com/ssonsal/football/review/dto/response/ReportResponseDto.java
+++ src/main/java/com/ssonsal/football/review/dto/response/ReportResponseDto.java
@@ -12,18 +12,22 @@ import java.time.LocalDate;
 @Builder
 public class ReportResponseDto {
     private Long id;
-    private Long reviewId;
+    private String review;
     private String reason;
     private int reportCode;
+    private int reviewDeleteCode;
+    private Long reviewId;
     private LocalDate createdAt;
     private LocalDate modifiedAt;
 
     public static ReportResponseDto fromEntity(Report report) {
         return new ReportResponseDtoBuilder()
                 .id(report.getId())
-                .reviewId(report.getReview().getId())
+                .review(report.getReview().getComment())
                 .reason(report.getReason())
                 .reportCode(report.getReportCode())
+                .reviewDeleteCode(report.getReview().getDeleteCode())
+                .reviewId(report.getReview().getId())
                 .createdAt(LocalDate.from(report.getCreatedAt()))
                 .modifiedAt(LocalDate.from((report.getModifiedAt())))
                 .build();
