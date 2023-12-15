de9f091817849180c6b4270997310d40a53ea2d6
joon <wnsgud0322@gmail.com>
2023-12-15 15:25:37 +0900

    test

---
+++ src/main/java/com/ssonsal/football/game/controller/MatchTeamController.java
@@ -0,0 +1,87 @@
+package com.ssonsal.football.game.controller;
+
+import com.ssonsal.football.game.dto.request.ApprovalTeamRequestDto;
+import com.ssonsal.football.game.dto.request.GameResultRequestDto;
+import com.ssonsal.football.game.dto.response.GameResultResponseDto;
+import com.ssonsal.football.game.exception.MatchErrorCode;
+import com.ssonsal.football.game.service.GameService;
+import com.ssonsal.football.game.service.MatchTeamService;
+import com.ssonsal.football.game.util.GameSuccessCode;
+import com.ssonsal.football.game.util.TeamResult;
+import com.ssonsal.football.game.util.Transfer;
+import com.ssonsal.football.global.exception.CustomException;
+import com.ssonsal.football.global.util.SuccessCode;
+import com.ssonsal.football.global.util.formatter.DataResponseBodyFormatter;
+import com.ssonsal.football.global.util.formatter.ResponseBodyFormatter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.ssonsal.football.game.util.GameConstant.CONFIRMED_GAME_ID;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/games")
+@Tag(name = "MatchTeam", description = "MatchTeam API")
+public class MatchTeamController {
+
+    private final GameService gameService;
+    private final MatchTeamService matchTeamService;
+
+    /**
+     * 신청한 팀 중 한 팀을 대상으로 승인하는 기능
+     * <p>
+     * 승-패, 무-무 외의 입력 시, 두 결과를 모두 null로 초기화 후 예외 발생
+     * 대기 중이거나 종료된 게임의 경우 예외 발생
+     * 상대 팀을 구하지 않은 게임의 경우 예외 발생
+     *
+     * @param gameId              url에서 가져오는 해당 게임의 식별자
+     * @param approvalAwayTeamDto 승인 대상 팀 정보
+     * @return 성공 코드와 해당 게임 아이디를 ResponseBody에 담아 반환
+     */
+    @PostMapping("/{gameId}/match-teams")
+    public ResponseEntity<ResponseBodyFormatter> approveAwayTeam(@PathVariable Long gameId,
+                                                                 @RequestBody ApprovalTeamRequestDto approvalAwayTeamDto) {
+
+        Long userId = 6L;
+        Long confirmedGameId = matchTeamService.approveAwayTeam(userId, gameId, approvalAwayTeamDto);
+
+        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, Transfer.longIdToMap(CONFIRMED_GAME_ID, confirmedGameId));
+    }
+
+
+
+
+    /**
+     * 상대팀을 구한 게임이 확정된 후, 각 팀에 경기 후 결과를 기입하는 기능
+     *
+     * @param gameId        url에서 가져오는 해당 게임의 식별자
+     * @param gameResultDto 확정된 게임에서 기입한 결과와 대상 팀
+     * @return 성공 코드와 해당 게임의 각각 두 팀의 result와 두 팀의 result를 더한 값을 ResponseBody에 담아 반환
+     */
+    @PostMapping("/{gameId}/match-teams/result")
+    public ResponseEntity<ResponseBodyFormatter> enterResult(@PathVariable Long gameId,
+                                                             @RequestBody GameResultRequestDto gameResultDto) {
+
+        Long userId = 1L;
+        GameResultResponseDto gameResult = gameService.enterResult(userId, gameId, gameResultDto);
+
+        return setHttpStatus(gameResult);
+    }
+
+    private ResponseEntity<ResponseBodyFormatter> setHttpStatus(GameResultResponseDto gameResult) {
+
+        if (gameResult.getTotalScore() == null) {
+            throw new CustomException(MatchErrorCode.IMPOSSIBLE_RESULT, gameResult);
+        }
+        if (gameResult.getTotalScore() < TeamResult.END.getScore()) {
+            return DataResponseBodyFormatter.put(GameSuccessCode.WAIT_FOR_ANOTHER_TEAM, gameResult);
+        }
+
+        return DataResponseBodyFormatter.put(SuccessCode.SUCCESS, gameResult);
+    }
+
+}
---
+++ src/main/java/com/ssonsal/football/game/service/MatchApplicantServiceImpl.java
@@ -0,0 +1,103 @@
+package com.ssonsal.football.game.service;
+
+import com.ssonsal.football.game.dto.request.MatchApplicationRequestDto;
+import com.ssonsal.football.game.entity.ApplicantStatus;
+import com.ssonsal.football.game.entity.Game;
+import com.ssonsal.football.game.entity.MatchApplication;
+import com.ssonsal.football.game.exception.GameErrorCode;
+import com.ssonsal.football.game.exception.MatchErrorCode;
+import com.ssonsal.football.game.repository.GameRepository;
+import com.ssonsal.football.game.repository.MatchApplicationRepository;
+import com.ssonsal.football.global.exception.CustomException;
+import com.ssonsal.football.global.util.ErrorCode;
+import com.ssonsal.football.team.entity.Team;
+import com.ssonsal.football.user.entity.User;
+import com.ssonsal.football.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.ssonsal.football.game.util.GameConstant.GAME_ID;
+import static com.ssonsal.football.game.util.GameConstant.USER_ID;
+import static com.ssonsal.football.game.util.Transfer.longIdToMap;
+import static com.ssonsal.football.global.util.ErrorCode.FORBIDDEN_USER;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MatchApplicantServiceImpl implements MatchApplicantService {
+
+    private final UserRepository userRepository;
+    private final GameRepository gameRepository;
+    private final MatchApplicationRepository matchApplicationRepository;
+
+    @Transactional
+    public Long applyToGameAsAway(Long gameId, Long userId, MatchApplicationRequestDto applicationTeamDto) {
+
+        // 공통 처리 고안 필수
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND, longIdToMap(USER_ID, userId)));
+        Team team = user.getTeam();
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST, longIdToMap(GAME_ID, gameId)));
+
+        checkWriterInTeam(team);
+        checkDuplicateApplication(team, game);
+
+        MatchApplication matchApplication = matchApplicationRepository.save(
+                MatchApplication.builder()
+                        .applicant(user)
+                        .team(team)
+                        .game(game)
+                        .applicationStatus(ApplicantStatus.WAITING.getDescription())
+                        .matchTeamDto(applicationTeamDto)
+                        .build()
+        );
+
+        return matchApplication.getId();
+    }
+
+    @Transactional
+    public Long rejectApplicationAsAway(Long userId, Long gameId, Long matchApplicationId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND, longIdToMap(USER_ID, userId)));
+
+        if (!gameRepository.existsByIdAndWriterEquals(gameId, user)) {
+            throw new CustomException(FORBIDDEN_USER);
+        }
+
+        MatchApplication matchApplication = matchApplicationRepository.findById(matchApplicationId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST));
+        matchApplication.reject();
+
+        return matchApplication.getId();
+    }
+
+
+    private void checkDuplicateApplication(Team team, Game game) {
+
+        if (Objects.equals(team, game.getHome())) {
+            log.info("해당 게임에 이미 확정된 팀입니다.");
+            throw new CustomException(MatchErrorCode.ALREADY_APPROVAL_TEAM);
+        }
+
+        List<MatchApplication> matchApplications = game.getMatchApplications();
+        long count = matchApplications.stream().filter(matchTeam -> matchTeam.getTeam().equals(team)).count();
+
+        if (count != 0) {
+            log.info("해당 게임에 이미 신청한 팀입니다.");
+            throw new CustomException(MatchErrorCode.ALREADY_APPLICANT_TEAM);
+        }
+    }
+
+    private void checkWriterInTeam(Team team) {
+        if (team == null) {
+            throw new CustomException(GameErrorCode.WRITER_NOT_IN_TEAM);
+        }
+    }
+}
